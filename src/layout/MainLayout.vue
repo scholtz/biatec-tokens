c4bab6e5b6ab0038b2f1d668f689be0e890d81fb
Ludovit Scholtz <ludko2@gmail.com>
2026-01-26 21:19:56 +0100

    Remove unused BoltBadge component and clean up MainLayout.vue

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import Navbar from "../components/layout/Navbar.vue";
 import Sidebar from "../components/layout/Sidebar.vue";
-import BoltBadge from "../components/ui/BoltBadge.vue";
 import ApiHealthBanner from "../components/ApiHealthBanner.vue";
 import { AlgorandAuthentication, type INotification } from "algorand-authentication-component-vue";
 const onNotification = (notification: INotification) => {
@@ -13,7 +12,7 @@ const onNotification = (notification: INotification) => {
   <AlgorandAuthentication @notification="onNotification" arc14Realm="BiatecTokens#ARC14">
     <div class="min-h-screen transition-colors duration-200">
       <Navbar />
-      
+
       <!-- API Health Banner -->
       <ApiHealthBanner />
 
@@ -25,15 +24,6 @@ const onNotification = (notification: INotification) => {
           </div>
         </main>
       </div>
-
-      <!-- Footer with Bolt.new badge -->
-      <footer class="lg:pl-64 py-6">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="flex items-center justify-center">
-            <BoltBadge />
-          </div>
-        </div>
-      </footer>
     </div>
   </AlgorandAuthentication>
 </template>
